8fc9a923745d44dae8564b33c8026c89b0649900
Paty R. Zablah <prz202@nyu.edu>
2015-01-30 17:13:54 -0500

    added python code

---
+++ getdata_nytimes.py
@@ -0,0 +1,102 @@
+'''
+Created on Jan 30, 2015
+IWMF Hackathon - The Gender Report
+Gets data from the NYTimes API
+@author: Mimi_2
+'''
+import urllib
+import urllib2
+import json
+import pprint
+
+search_params = {}
+#q     Search query term. Search is performed on the article body, headline and byline.
+#search_params['q'] = ''
+#fq     Filtered search query using standard Lucene syntax.
+#search_params['fq'] = 'section_name:("Front Page")'
+search_params['fl'] = 'byline,print_page,news_desk,keywords,pub_date'
+search_params['sort']='newest'
+#search_params['api-key'] = 'b289f107938f18d0bb765d99c4a7f52a:14:71126476'
+search_params['api-key'] = 'fc509326b88c497dd36d9f8939c9f1eb:13:71126316'
+
+#URL for the gender API to determine gender of first names
+genderapi = 'http://api.genderize.io?name='
+genderapi2 = 'https://gender-api.com/get'
+
+'''
+#control page numbers - note that currently pages are not allowed over 100... so need to search with begin and end date
+for i in range (1,101):
+    #initialize articles list
+    articles = []
+    search_params['page']=str(i)
+    url_params = urllib.urlencode(search_params)
+    print url_params
+    api_url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json'
+    
+    try: results = urllib2.urlopen(api_url + '?' + url_params)
+    except urllib2.URLError as e:
+        print e.reason
+        
+    data = json.load(results)
+    docs = data['response']['docs']
+    
+    for doc in docs:
+        article = {}
+        if doc['byline'] is not None:
+            if len(doc['byline']) > 0 and len(doc['byline']['person']) >0:
+                firstname =  doc['byline']['person'][0]['firstname']
+                article['firstname'] = firstname
+                article['pub_date'] = doc['pub_date']
+                article['news_desk'] = doc['news_desk']
+                article['gender'] = json.load(urllib2.urlopen(genderapi2 + '?name=' + firstname + '&key=QWGXFHAPcQaqfoAewt'))['gender']
+                #json.load(urllib2.urlopen(genderapi + firstname))['gender']
+                article['print_page'] = doc['print_page']
+                print article
+                articles.append(article)
+    if len(articles) > 0:
+        json_string = json.dumps(articles)
+        with open('json_dump2.txt', 'a') as f:
+            f.write(json_string)
+    del articles[:]
+
+'''
+#Block to get next 100 articles based on end date... from last block
+#to improve the code, you can use dynamic end_date within the forloop so you do not need to exceed 100 pages...
+for i in range (1,101):
+    #initialize articles list
+    articles = []
+    search_params['end_date'] = '19800130'
+    search_params['page']=str(i)
+    url_params = urllib.urlencode(search_params)
+    print url_params
+    api_url = 'http://api.nytimes.com/svc/search/v2/articlesearch.json'
+    
+    try: results = urllib2.urlopen(api_url + '?' + url_params)
+    except urllib2.URLError as e:
+        print e.reason
+        
+    data = json.load(results)
+    docs = data['response']['docs']
+    
+    for doc in docs:
+        article = {}
+        if doc['byline'] is not None:
+            if 'person' in doc['byline']:
+                if len(doc['byline']) > 0 and len(doc['byline']['person']) >0 and 'firstname' in doc['byline']['person'][0]:
+                    firstname =  doc['byline']['person'][0]['firstname']
+                    article['firstname'] = firstname
+                    article['pub_date'] = doc['pub_date']
+                    article['news_desk'] = doc['news_desk']
+                    article['gender'] = json.load(urllib2.urlopen(genderapi2 + '?name=' + firstname + '&key=QWGXFHAPcQaqfoAewt'))['gender']
+                    #json.load(urllib2.urlopen(genderapi + firstname))['gender']
+                    article['print_page'] = doc['print_page']
+                    print article
+                    articles.append(article)
+    if len(articles) > 0:
+        json_string = json.dumps(articles)
+        with open('json_dump2.txt', 'a') as f:
+            f.write(json_string)
+    del articles[:]
+
+
+
---
+++ main.js
@@ -0,0 +1,126 @@
+//Create the empty array that will include the male data and the female data 
+ 
+var totalMenFront = new Array(); 
+var firstNames = [];
+
+$(document).ready(function(){
+	init(); 
+	//display(); 
+}); 
+
+function init() {
+	var totalWomenFront = new Array();
+}
+
+function showData1() {
+	console.log("show the canvas");
+
+	if(document.getElementById("graph").style.display == 'none')  {
+		document.getElementById("graph").style.display='block';
+	}
+
+	else
+	{
+	document.getElementById("graph").style.display = 'none';
+	}
+	
+}; 
+
+
+function showData2() {
+	alert("show the canvas2");
+	console.log("this is data2"); 
+	
+}; 
+
+
+
+function getDataNYT() {
+	console.log("I am in the process and about to call the ajax function");
+	$.ajax({
+		url:"http://api.nytimes.com/svc/search/v2/articlesearch.json?fl=byline&facet_field=section_name&begin_date=20100101&end_date=20150130&api-key=fc509326b88c497dd36d9f8939c9f1eb:13:71126316",
+		type: 'GET', 
+		crossDomain:"true",
+			success:function(response){
+			alert("success");
+			console.log("I can call the API"); 
+			console.log(response); 
+
+			//var firstNames = [];
+			for (var i = 0; i < response.response.docs.length; i++) {
+				var doc = response.response.docs[i];
+				//if there is a person that wrote this:
+				if (doc.byline && doc.byline.person && doc.byline.person[0]){
+					firstNames.push(doc.byline.person[0].firstname);
+				}
+			}
+			//print out the first Names
+			console.log(firstNames);
+		  	showData(); 
+		  	//make a JSON Object 
+			}, //end of success function
+
+		error:function(x,e){
+			if(x.status==0){
+				alert('You are offline!!\n Please Check Your Network.');
+				}else if(x.status==404){
+					alert('Requested URL not found.');
+				}else if(x.status==500){
+					alert('Internel Server Error.');
+				}else if(e=='parsererror'){
+					alert('Error.\nParsing JSON Request failed.');
+				}else if(e=='timeout'){
+					alert('Request Time out.');
+				}else {
+					alert('Unknow Error.\n'+x.responseText);
+				}
+			}
+		}); 
+	}
+
+
+
+
+
+function getWPData() {
+	console.log("calling WP"); 
+        var params = { 
+            // Specify your developer key 
+            key: 'F75F9692-6BA5-4950-A227-78F09670392E', 
+            // Specify values for optional parameters, as needed 
+            //content_set: , 
+            //callback: , 
+        }; 
+          
+        $.ajax({ 
+            // Specify values for path parameters (shown as {...})  
+            url: 'http://api.washingtonpost.com/trove/v1/channels/result?F75F9692-6BA5-4950-A227-78F09670392E', 
+            type: 'GET', 
+            crossDomain:"true",
+            beforeSend: function (xhr) { 
+                debugger; 
+                xhr.overrideMimeType("application/json"); 
+            } 
+        }) 
+        .done(function(data) { 
+            alert("success"); 
+        }) 
+        .fail(function() { 
+            alert("error"); 
+        }); 
+    }; 
+
+function showData() {
+
+	for (var i = 0 ; i< firstNames.length; i ++) {
+		console.log("went through this" + i + "times");
+		//replacing the whole thing
+		//$('#nytwomen').html('<span>' + firstNames[i] +'<span><br>');
+		//show the total  number: 
+		$('#nytwomen').html('<span>' + firstNames.length +'<span><br>');
+		console.log(firstNames.length); 
+	}; 
+
+}
+
+
